a178aed05a470a4f6915a8a3b1a15e7bd6f15d36
Anshcodeify45 <anishpatnaik45@gmail.com>
2024-04-22 14:29:54 +0530

    third commit

--- ebook-site/src/Components/Navbar.jsx
+++ ebook-site/src/Components/Navbar.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
-import { Typography ,styled ,Toolbar} from '@mui/material';
-
+import { Typography ,styled ,Toolbar, Box} from '@mui/material';
+import {Link} from 'react-router-dom'
+import Logo from '../Assets/logo.png'
 
 
 const Nav = styled(AppBar)`
+    display:flex;
     background:gray;
-    text-align:center;
+    
 `
 const Tool = styled(Toolbar)`
-    margin-left:30%;
+    & > div{
+      text-align:start;
+      margin-right:28%;
+    }
 `
 const Tabs = styled(Typography)`
     font-size:20px;
     margin-right:30px;
-    
 `
+
 function Navbar() {
   return (
     <Nav position='static'>
-    <Tool>
+        <Tool>
+        <Box>
+        <Link to={'/'}>
+          <img src={Logo} alt="logo" style={{width:75}}/>
+        </Link>
+        </Box>
         <Tabs>Home</Tabs>
         <Tabs>About</Tabs>
         <Tabs>Pages</Tabs>
         <Tabs>Shope</Tabs>
         <Tabs>Artcles</Tabs>
         <Tabs>Contact</Tabs>
-      </Tool>
+        </Tool>
     </Nav>
   )
 }
